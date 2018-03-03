0ffe4c14f793b8413a841faaee6022c95dc79c78
Thibault Guegan <thibault.guegan@gmail.com>
2018-03-03 10:08:59 +0000

    Add domain tests

--- app/src/main/java/fr/northborders/trendingrepos/data/data/GithubService.kt
+++ app/src/main/java/fr/northborders/trendingrepos/data/data/GithubService.kt
@@ -1,4 +1,4 @@
-package fr.northborders.trendingrepos.data.repository
+package fr.northborders.trendingrepos.data.data
 
 import fr.northborders.trendingrepos.data.model.RepoEntity
 import io.reactivex.Single
@@ -7,5 +7,5 @@ import retrofit2.http.Query
 
 interface GithubService {
     @GET("search/repositories")
-    fun getRepos(@Query("q") query: String, @Query("page") pageId: Int): Single<List<RepoEntity>>
+    fun getRepos(@Query("q") query: String): Single<List<RepoEntity>>
 }
