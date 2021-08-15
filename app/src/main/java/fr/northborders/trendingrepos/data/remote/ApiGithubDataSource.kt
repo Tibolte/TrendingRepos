e451911789d95bd83c0ab0f69bd5e9c911ca4f24
Thibault <thibault@travelloapp.com>
2021-08-15 14:27:19 +1000

    Remove old files

--- app/src/main/java/fr/northborders/trendingrepos/data/remote/ApiGithubDataSource.kt
+++
@@ -1,9 +0,0 @@
-package fr.northborders.trendingrepos.data.remote
-
-import javax.inject.Inject
-
-open class ApiGithubDataSource @Inject constructor(private val githubService: GithubService, private val markdownService: MarkdownService) {
-    fun getRepos(query: String, sort: String, page: Int) = githubService.getRepos(query, sort, page)
-    fun getReadme(owner: String?, repoName: String?) = githubService.getReadme(owner, repoName)
-    fun markdown(readme: String) = markdownService.markdown(readme)
-}
