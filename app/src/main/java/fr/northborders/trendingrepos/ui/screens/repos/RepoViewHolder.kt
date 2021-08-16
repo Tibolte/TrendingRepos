746462838e8fc65af11f1b511b5de0d7207e6ef6
Thibault Guegan <thibault@travelloapp.com>
2021-08-16 11:24:33 +1000

    Use Coil-kt image library

--- app/src/main/java/fr/northborders/trendingrepos/ui/screens/repos/RepoViewHolder.kt
+++ app/src/main/java/fr/northborders/trendingrepos/ui/screens/repos/RepoViewHolder.kt
@@ -2,14 +2,14 @@ package fr.northborders.trendingrepos.ui.screens.repos
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
+import coil.load
 import fr.northborders.trendingrepos.ui.model.RepoViewModel
 import fr.northborders.trendingrepos.ui.screens.repodetail.view.RepoDetailActivity
 import kotlinx.android.synthetic.main.item_repo.view.*
 
 class RepoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun renderRepo(repoViewModel: RepoViewModel) {
-        Picasso.get().load(repoViewModel.owner.avatarUrl).into(itemView.repo_owner_avatar)
+        itemView.repo_owner_avatar.load(repoViewModel.owner.avatarUrl)
         itemView.repo_name.text = repoViewModel.name
         itemView.repo_desc.text = repoViewModel.description
         itemView.repo_stars.text = String.format("Stars: %d", repoViewModel.starGazersCount)
